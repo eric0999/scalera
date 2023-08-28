16165bf494d9a4adc9b2d83c87de18cf416314c0
Eric Broñola <eric.bronola@mgenesis.local>
2023-08-28 17:54:59 +0800

    'second'

--- server.js
+++ server.js
@@ -2,21 +2,32 @@ const http = require('node:http');
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+//app.use(express.static('public'))
+
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use(express.urlencoded({extended: true}));
+const hostname = '192.168.100.4';
+const port = 3000;
+
+
 app.get('/', (req, res) =>{
   res.render('index')
 })
 app.get('/scalera', (req, res) =>{
   res.render('scalera')
 })
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json()
 
-app.post('/add', function(req,res) {
-  console.log(res);
-  //res.json(req.body);
+const aaa = (req, res, next) => {
+  console.log('aaa')
+  next()
+}
+
+app.post('/add', jsonParser, aaa,  function(req,res) {
+  console.log(req.body)
+  res.render('scalera_board', {data:req.body})
 });
 
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
-const hostname = '192.168.254.108';
-const port = 3000;
-const server = app.listen(port, hostname)
+app.listen(port, hostname)
